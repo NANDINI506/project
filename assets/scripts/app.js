34431c048e6213bfce32db8e443f6fa9337c00aa
NANDINI506 <nandinianil1352@gmail.com>
2025-03-27 12:09:47 +0530

    complete calculator 1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,41 +1,43 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//get user input from input field
 function getUserNumberInput() 
 {
   return parseInt(userInput.value);
 }
 
+//generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber)
 {
   const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription);
+  outputResult(currentResult, calcDescription);//from vendor.js
 }
 function add() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
 function subtract() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -=  enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteOutput('/', initialResult, enteredNumber);
 }
 
