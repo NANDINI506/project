bd0499f9fc74f230d3db245a840e86e35a94abf5
NANDINI506 <nandinianil1352@gmail.com>
2025-03-27 10:36:17 +0530

    new project changed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@ let currentResult = defaultResult;
 
 function add() {
   const calcDescription = `${currentResult} + ${userInput.value}`;
-  currentResult = currentResult + parseInt{userInput.value};
+  currentResult = currentResult + parseInt(userInput.value);
   outputResult(currentResult, calcDescription);
 }
 
